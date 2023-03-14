91d36b147eceac51b4f7b24a8e4c27936cd063e3
Jason Smale <jason.smale@gmail.com>
2023-03-14 23:33:21 +1100

    switching from rails site to static site

--- app/controllers/programs_controller.rb
+++
@@ -1,5 +0,0 @@
-class ProgramsController < ApplicationController
-  def view
-    @exercises = Exercise.where(:id => params[:exercises].map(&:to_i))
-  end
-end
--- db/migrate/20121119062435_add_image_to_exercises.rb
+++
@@ -1,9 +0,0 @@
-class AddImageToExercises < ActiveRecord::Migration
-  def up
-    add_attachment :exercises, :image
-  end
-
-  def down
-    remove_attachment :exercises, :image
-  end
-end
